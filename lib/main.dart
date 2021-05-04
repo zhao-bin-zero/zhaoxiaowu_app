4807ef30128d4ffc9fb9ec4023d58efac349bbe1
王众 <wz@dctech.cn>
2021-05-04 11:05:49 +0800

    极光推送

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:jpush_flutter/jpush_flutter.dart';
 import 'package:zhaoxiaowu_app/routes/routes.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final JPush jpush = new JPush();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String platformVersion;
+    const bool inProduction = const bool.fromEnvironment("dart.vm.product");
+    try {
+      jpush.addEventHandler(
+          onReceiveNotification: (Map<String, dynamic> message) async {
+        print("flutter onReceiveNotification: $message");
+      }, onOpenNotification: (Map<String, dynamic> message) async {
+        print("flutter onOpenNotification: $message");
+      }, onReceiveMessage: (Map<String, dynamic> message) async {
+        print("flutter onReceiveMessage: $message");
+      }, onReceiveNotificationAuthorization:
+              (Map<String, dynamic> message) async {
+        print("flutter onReceiveNotificationAuthorization: $message");
+      });
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+    print("开发模式" + inProduction.toString());
+    jpush.setup(
+      appKey: "e700e2560560848cdeb0b283", //你自己应用的 AppKey
+      channel: "developer-default",
+      production: inProduction,
+      debug: inProduction, //debug日志 true=打印
+    );
+    jpush.applyPushAuthority(
+        new NotificationSettingsIOS(sound: true, alert: true, badge: true));
+
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    jpush.getRegistrationID().then((rid) {
+      print("flutter get registration id : $rid");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
